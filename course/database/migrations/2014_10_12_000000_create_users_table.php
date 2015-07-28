0fa51d78cfb62434315c8f062cca03305ebdb307
djmiguelarango <Miguel@djmiguelarango.local>
2015-07-27 22:18:59 -0400

    Migraciones en Laravel 5

--- course/database/migrations/2014_10_12_000000_create_users_table.php
+++ course/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
+			$table->string('full_name', 20);
+			$table->string('last_name', 20);
 			$table->string('email')->unique();
 			$table->string('password', 60);
+			$table->boolean('active')->default(true);
 			$table->rememberToken();
 			$table->timestamps();
 		});
